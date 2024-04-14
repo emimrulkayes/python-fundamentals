327b4329a9c28ed47a5bf6bc66ad56af70c15e6f
emimrulkayes <imrul.xs@gmail.com>
2024-04-15 03:19:03 +0600

    added variable file

---
+++ function-variables/variables.py
@@ -0,0 +1,12 @@
+# Ask the user for their name
+name = input("What's your name? ")
+
+# Print hello and the inputted name
+print("hello,", name)
+
+
+
+# Probably the most elegant way to use strings would be as follows:
+# Ask the user for their name
+name = input("What's your name? ")
+print(f"hello, {name}")
